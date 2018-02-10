fa7d0ae213c442055a8cdb9ae05dbd228fa9a5e6
Danny Koester <dkoester306@gmail.com>
2018-02-10 18:54:08 -0500

    Background Scrolling Functional
    Note to self: The implementation could be better, so try to improve it in the future.

--- Assets/Sprites/BackgroundScrolling.cs
+++ Assets/Sprites/BackgroundScrolling.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundScrolling : MonoBehaviour {
+	public float backgroundSize;
 
-	// Use this for initialization
-	void Start () {
-		
+	private Transform m_CameraTransform;
+	private Transform[] m_Layers;
+	private float m_ViewZone=10;
+	private int m_LeftIndex;
+	private int m_RightIndex;
+
+	[SerializeField] private float m_MaxScrollingSpeed = 0.3f;
+
+	void Start()
+	{
+		m_CameraTransform = Camera.main.transform;
+		m_Layers = new Transform[transform.childCount];
+		for(int i =0;i<transform.childCount;i++)
+			m_Layers [i] = transform.GetChild (i);
+
+		m_LeftIndex = 0;
+		m_RightIndex = m_Layers.Length - 1;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update()
+	{
+		MoveBackgrounds ();
+		if (m_CameraTransform.position.x > (m_Layers [m_LeftIndex].transform.position.x + m_ViewZone))
+			ScrollLeft ();
 	}
+
+	private void ScrollLeft()
+	{
+		int lastLeft = m_LeftIndex;
+		Vector3 newPos = m_Layers [m_LeftIndex].position;
+		newPos.x = m_Layers [m_RightIndex].position.x + m_Layers [m_LeftIndex].GetComponent<SpriteRenderer> ().bounds.size.x;
+		m_Layers [m_LeftIndex].position = newPos;
+		m_LeftIndex = m_RightIndex;
+		m_RightIndex--;
+		if (m_RightIndex < 0)
+			m_RightIndex = m_Layers.Length - 1;
+
+		Debug.Log (m_LeftIndex+"  "+m_RightIndex);
+
+	}
+
+	void MoveBackgrounds()
+	{
+		for(int i =0;i<m_Layers.Length;i++)
+		{
+			Vector3 newPos = m_Layers [i].transform.position;
+			newPos.x -= m_MaxScrollingSpeed;
+			m_Layers [i].transform.position = newPos;
+		}
+	}
+
 }
