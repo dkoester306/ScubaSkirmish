12ad34f2ced265c52e9117197ee9957e6cfa258a
Danny Koester <dkoester306@gmail.com>
2018-05-20 16:24:42 -0500

    Added functional buttons for main menu

---
+++ Assets/Scripts/MenuButtonHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadHelpMenu()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
