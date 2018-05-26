d39037449054507dd8f188dc16f1e91c9cb0ef76
Caleb Vaccaro <CalebVbusiness@gmail.com>
2018-05-25 20:46:59 -0400

    UI Title Screen Assets

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,40 @@
+﻿using System; 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour {
+
+    public Slider musicBar;
+    public bool musicplay;
+    public float musicVolume;
+
+	// Use this for initialization
+	void Awake () {
+        musicplay = true;
+        musicVolume = musicBar.value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SliderEqualsVolume();
+	}
+
+    public void CheckMute()
+    {
+        if (musicplay)
+        {
+            musicplay = !musicplay;
+        }
+        else
+        {
+            musicplay = !musicplay;
+        }
+    }
+
+    private void SliderEqualsVolume()
+    {
+        musicVolume = musicBar.value;
+    }
+}
