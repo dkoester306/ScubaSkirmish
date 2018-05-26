0623cff4c8d047b1ba16066788bb815ebb7a9ce6
Danny Koester <dkoester306@gmail.com>
2018-05-25 21:45:58 -0500

    Added sound effect when damaged

---
+++ Assets/DamageSoundBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSoundBehavior : MonoBehaviour {
+
+	AudioSource damageSound;
+
+	void Awake()
+	{
+		damageSound = GetComponent<AudioSource>();
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag=="Anchor"||other.tag=="Mine")
+		{
+			damageSound.Play();
+		}
+	}
+}
